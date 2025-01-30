70b574d9e80de19d914dfbb7d87922c0643a96b6
Zosha Chung <czosha@gmail.com>
2025-01-30 16:20:53 -0500

    tasks 1-5 of in class assignment

---
+++ in_class_assignment.js
@@ -0,0 +1,13 @@
+// Task 1
+
+
+let expenses = 7000; // Declare variable
+
+
+if (expenses > 7000) {
+   //if expenses are over 7000 -> High
+   console.log("High expenses")
+} else {
+   //if expenses otherwise -> Manageable
+   console.log("Manageable Expenses");
+}
