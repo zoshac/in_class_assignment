b33a386c4f2032e8795d87ee846368b8bd937bfd
Zosha Chung <czosha@gmail.com>
2025-01-30 16:38:47 -0500

    Assignment completed

--- in_class_assignment.js
+++ in_class_assignment.js
@@ -1,6 +1,5 @@
 // Task 1
 
-
 let expenses = 7000; // Declare variable
 
 
@@ -11,3 +10,40 @@ if (expenses > 7000) {
    //if expenses otherwise -> Manageable
    console.log("Manageable Expenses");
 }
+//Task 2
+
+
+for (let i = 10; i>=1; i--) {
+    console.log(i);
+ }
+ 
+ 
+ //Task 3
+ 
+ 
+ let num = 2;
+ while (num <= 10) {
+    console.log(num);
+    num+= 2;
+ }
+ //Task 4
+
+
+function calculateSquare(num) {
+    return num * num
+ }
+ console.log(square(3))
+ 
+ 
+ //Task 5
+ 
+ 
+ const doubleNumber = num => (num * 2);
+ console.log(doubleNumber(3));
+ 
+ 
+ //Task 6
+ const numbers = [3, 6, 9];
+ const doubleNumber = numbers.map (number * 2);
+ console.log(doubled);
+ 
